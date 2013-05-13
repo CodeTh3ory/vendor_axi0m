c54384b65694748361d62f6846d848e0b2e4a743
CodeTh3ory <codeth3ory@gmail.com>
2013-05-13 08:27:20 -0400

    Add Toroplus to lunch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
-add_lunch_combo axi0m_maguro-userdebug
 add_lunch_combo axi0m_mako-userdebug
+add_lunch_combo axi0m_maguro-userdebug
 add_lunch_combo axi0m_toro-userdebug
+add_lunch_combo axi0m_toroplus-userdebug
 add_lunch_combo axi0m_xt926-userdebug
 add_lunch_combo axi0m_i605-userdebug
