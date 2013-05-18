3a6223528da5135ecbc49a9aa7ed8714cff23f76
CodeTh3ory <codeth3ory@gmail.com>
2013-05-18 19:44:56 -0400

    Add Tablet mk

---
+++ config/common_tablet.mk
@@ -0,0 +1,20 @@
+# audio
+include frameworks/base/data/sounds/NewAudio.mk
+
+# common
+$(call inherit-product, vendor/axi0m/config/common.mk)
+
+# gps
+PRODUCT_COPY_FILES += \
+    vendor/axi0m/prebuilt/common/etc/gps.conf:system/etc/gps.conf
+
+# bluetooth
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
+
+# ringtones
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Hydra.ogg \
+    ro.config.notification_sound=Proxima.ogg \
+    ro.config.alarm_alert=Cesium.ogg
+
