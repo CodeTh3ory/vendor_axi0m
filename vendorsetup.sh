6923d08aa624860e5159682ea19b652ff648e553
AXI0M <codeth3ory@gmail.com>
2013-06-15 09:55:27 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
-add_lunch_combo axi0m_m7-userdebug
+add_lunch_combo axi0m_m7tmo-userdebug
+add_lunch_combo axi0m_m7att-userdebug
 add_lunch_combo axi0m_mako-userdebug
 add_lunch_combo axi0m_maguro-userdebug
 add_lunch_combo axi0m_toro-userdebug
